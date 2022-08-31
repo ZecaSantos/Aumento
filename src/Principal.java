b58b3a3404543e01970b2be59b2c4d6b974138e7
ZecaSantos <zecadeveloper@gmail.com>
2022-08-31 14:14:30 -0300

    Primeira Edição

---
+++ src/Principal.java
@@ -0,0 +1,60 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double sal_atual, novo_sal, aumento; 
+		int porcentagem;
+		
+		aumento = 0;
+		novo_sal = 0;
+		sal_atual = 0;
+		porcentagem = 0;
+		
+		System.out.print("Digite o salário da pessoa: ");
+		sal_atual = sc.nextDouble();
+		
+	
+		if(sal_atual <= 1000) {
+			porcentagem = 20;
+			aumento = sal_atual * porcentagem / 100;
+			novo_sal = sal_atual + aumento;
+
+		}
+		
+		else if(sal_atual <= 3000) {
+			porcentagem = 15;
+			aumento = sal_atual * porcentagem / 100;
+			novo_sal = sal_atual + aumento;
+
+		}
+		
+		else if(sal_atual <= 8000.0) {
+			porcentagem = 10;
+			aumento = sal_atual * porcentagem / 100;
+			novo_sal = sal_atual + aumento;
+
+		}
+		
+		else {
+			porcentagem = 5;
+			aumento = sal_atual * porcentagem / 100;
+			novo_sal = sal_atual + aumento;
+		}
+		
+		System.out.println();
+		System.out.printf("Novo Salário: R$ %.2f\n", novo_sal);
+		System.out.printf("Aumento: R$ %.2f\n", aumento);		
+		System.out.printf("Porcentagem: %d %%\n", porcentagem);
+		
+		
+		sc.close();
+
+	}
+
+}
